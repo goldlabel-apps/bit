15ae0f11af6a9941706eec81c5250d7d00f18abc
listingslab <listingslab@gmail.com>
2022-11-24 13:40:30 +0100

    @dt9affiliations/atracktui.ui.title

--- my-bit-test/src/App.tsx
+++ my-bit-test/src/App.tsx
@@ -2,11 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import {Title} from "@dt9affiliations/atracktui.ui.title";
 
 function App() {
   return (
     <div className="App">
+        <div style={{background: "#eee", padding: 8}}>
+          <pre style={{textAlign: "left"}}>
+            {`import {Title} from "@dt9affiliations/atracktui.ui.title";`}
+          </pre>
+          <pre style={{textAlign: "left"}}>
+            {`<Title title="Test Title" subtitle="Test Subtitle"/>`}
+          </pre>
+        </div>
+
       <header className="App-header">
+
+        <div style={{border: "1px solid #eee"}}>
+          <Title title="Test Title" subtitle="Test Subtitle"/>
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
